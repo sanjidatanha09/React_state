dc1c84fde7ea0458a8286502b00f72ec6e3366d7
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-08 19:42:20 +0600

    use state add for team counter

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,11 @@ function App() {
   return (
     <>
       
-      
+      <div>
+        <h1>players : {team}</h1>
+        <button onClick={handleAdd}>Add</button>
+        <button onClick={handleAdd}>Add</button>
+      </div>
       
       
     </>
