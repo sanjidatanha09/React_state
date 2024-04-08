21b10fd627799a5bc7d8b48fad1a9c3367c4fd7d
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-08 19:46:59 +0600

    style add in div

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const {team, setTeam} = useState(0)
+
+  const handleAdd = () => {
+
+  }
+  const handleRemove = () =>{
+    
+  }
+
+  const teamStyle = {
+
+    border:'2px solid purple',
+    margin:'15px',
+    padding: '15px',
+    borderRadius: '15px'
+  }
   
 
   return (
     <>
       
-      <div>
+      <div style={teamStyle}>
         <h1>players : {team}</h1>
         <button onClick={handleAdd}>Add</button>
-        <button onClick={handleAdd}>Add</button>
+        <button onClick={handleRemove}>Remove</button>
       </div>
       
       
