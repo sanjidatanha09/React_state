2ecf2dc4bc4ddde743f1e95e7c341de744497aa9
Sanjida Akter <sanjidatanha09@gmail.com>
2024-04-09 12:27:29 +0600

    show the bottles lenght

--- src/Bottles.jsx
+++ src/Bottles.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Bottles = () => {
+    const [bottles, setBottles] = useState([])
+
+    useEffect( () =>{
+        fetch('fake.json')
+        .then(res =>res.json())
+        .then(data =>setBottles(data))
+    })
     return (
         <div>
-            <h2>Bottles here</h2>
+            <h2>Bottles here :{bottles.length}</h2>
         </div>
     );
 };
